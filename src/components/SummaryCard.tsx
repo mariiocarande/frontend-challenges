086c86f82563233a36ac55503184340ff3d2813c
Mario Carande Scarafia <mdc.mariio@gmail.com>
2023-06-25 19:41:28 -0300

    ADD: button component

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import items from "../data.json";
+import Button from "./Button";
 
 const SummaryCard = () => (
-  <div className="bg-white text-dark-gray-blue p-8 rounded-2xl">
+  <div className="bg-white text-dark-gray-blue p-8 rounded-2xl w-80">
     <h3 className="font-semibold text-lg">Summary</h3>
 
     {/* TODO: replace with SummaryItem */}
@@ -11,6 +12,8 @@ const SummaryCard = () => (
         <li key={item.category}>{item.score}</li>
       ))}
     </ul>
+
+    <Button title="Continue" />
   </div>
 );
 
