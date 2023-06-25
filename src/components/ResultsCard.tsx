6cc3a1d9882352cb0e006881f3e46a71cf20e399
Mario Carande Scarafia <mdc.mariio@gmail.com>
2023-06-25 18:27:35 -0300

    UPDATE: results card styles

--- src/components/ResultsCard.tsx
+++ src/components/ResultsCard.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 const ResultsCard = () => (
-  <div className="bg-gradient-to-b from-light-slate-blue to-light-royal-blue">
-    <h3>Your Result</h3>
-    <div>
-      <div>76</div>
-      <div>of 100</div>
+  <div className="flex flex-col items-center bg-gradient-to-b from-light-slate-blue to-light-royal-blue rounded-xl p-4">
+    <h3 className="text-light-lavender font-semibold">Your Result</h3>
+
+    <div className="flex flex-col items-center bg-gradient-to-b from-violet-blue to-persian-blue rounded-full w-40 p-10 mb-6 mt-6">
+      <div className="text-6xl font-bold text-pale-blue">76</div>
+      <div className="text-light-lavender text-sm font-semibold opacity-60">
+        of 100
+      </div>
     </div>
-    <h2>Great</h2>
-    <p>You scored higher than 65% of the people who have taken these tests.</p>
+
+    <h2 className="text-2xl font-semibold mb-2">Great</h2>
+    <p className="text-light-lavender text-sm w-60 text-center">
+      You scored higher than 65% of the people who have taken these tests.
+    </p>
   </div>
 );
 
