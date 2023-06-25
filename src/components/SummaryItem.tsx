4fdab2213b4cfd2a68b88818c92eb34dcc187b4d
mariiocarande <mdc.mariio@gmail.com>
2023-06-25 19:44:53 -0300

    Add custom button for categories

--- src/components/SummaryItem.tsx
+++ src/components/SummaryItem.tsx
@@ -1,39 +1,51 @@
+import classNames from "classnames";
+import Image from "next/image";
 import React from "react";
+import iconReaction from "../../public/assets/icon-reaction.svg";
+import iconMemory from "../../public/assets/icon-memory.svg";
+import iconVisual from "../../public/assets/icon-visual.svg";
+import iconVerbal from "../../public/assets/icon-verbal.svg";
 
-enum Variant {
-  reaction = "Reaction",
-  memory = "Memory",
-  verbal = "Verbal",
-  visual = "Visual",
-}
+type Category = "reaction" | "memory" | "verbal" | "visual";
 
 interface Props {
-  variant: Variant;
-  title?: string;
-  icon?: string;
-  number?: number;
+  category: Category;
+  title: string;
+  icon: string;
+  score: number;
 }
 
-const variants = {
-  reaction: {
-    className: "text-color-light-red bg-light-red",
-  },
-  memory: {
-    className: "text-color-orange-yellow bg-orange-yellow",
-  },
-  verbal: {
-    className: "text-color-green-teal bg-green-teal",
-  },
-  visual: {
-    className: "text-color-cobalt-blue bg-cobalt-blue",
-  },
+const variants: { [name in Category]: string } = {
+  reaction: "font-bold text-light-red bg-light-red-95",
+  memory: "font-bold text-orangey-yellow bg-orangey-yellow-95",
+  verbal: "font-bold text-green-teal bg-green-teal-95",
+  visual: "font-bold text-cobalt-blue bg-cobalt-blue-95",
 };
 
-const SummaryItem: React.FC<Props> = ({ title, variant, icon, number }) => (
-  <div className="flex p-4">
-    <div>{icon}</div>
-    <div>{title}</div>
-    <div className="text-black">{number}</div>
+const SummaryItem: React.FC<Props> = ({
+  title,
+  category = "reaction",
+  icon,
+  score,
+}) => (
+  <div
+    className={classNames(
+      "flex justify-between p-4 w-56 rounded-lg",
+      variants[category]
+    )}
+  >
+    <div className="flex gap-2">
+      <Image
+        src={iconReaction}
+        alt="icon category"
+        className={classNames(variants[category])}
+      />
+      <div className={classNames(variants[category])}>{title}</div>
+    </div>
+    <div className="flex gap-2 text-black">
+      <span className="font-bold">{score}</span>
+      <span className="flex text-dark-gray-blue">/ 100</span>
+    </div>
   </div>
 );
 
--- src/stories/SummaryItem.stories.tsx
+++ src/stories/SummaryItem.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
 import SummaryItem from "../components/SummaryItem";
 
 const meta: Meta<typeof SummaryItem> = {
@@ -11,6 +10,42 @@ export default meta;
 
 type Story = StoryObj<typeof SummaryItem>;
 
-export const Primary: Story = {
-  render: () => <SummaryItem />,
+export const Reaction: Story = {
+  args: {
+    title: "Reaction",
+    icon: "👍",
+    category: "reaction",
+    score: 80,
+  },
+  render: ({ ...args }) => <SummaryItem {...args} />,
+};
+
+export const Memory: Story = {
+  args: {
+    title: "Memory",
+    icon: "👍",
+    category: "memory",
+    score: 92,
+  },
+  render: ({ ...args }) => <SummaryItem {...args} />,
+};
+
+export const Visual: Story = {
+  args: {
+    title: "Visual",
+    icon: "👍",
+    category: "visual",
+    score: 61,
+  },
+  render: ({ ...args }) => <SummaryItem {...args} />,
+};
+
+export const Verbal: Story = {
+  args: {
+    title: "Verbal",
+    icon: "👍",
+    category: "verbal",
+    score: 72,
+  },
+  render: ({ ...args }) => <SummaryItem {...args} />,
 };
